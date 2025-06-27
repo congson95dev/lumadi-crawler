be49f508aa2671b8cacdc973a7d35619fde5b18f
Thomas Nguyen <thomas.nguyen1@vmogroup.com>
2025-06-27 14:55:42 +0700

    feat: update

--- rightmove-crawler.js
+++ rightmove-crawler.js
@@ -44,6 +44,7 @@ const RIGHTMOVE_URL = process.env.RIGHTMOVE_URL;
     });
 
     const urls = [];
+    const per_page = 24;
 
     // Đầu tiên kiểm tra xem pagination có tồn tại không
     const paginationElement = await page.$(
@@ -79,7 +80,7 @@ const RIGHTMOVE_URL = process.env.RIGHTMOVE_URL;
     let m = 0;
     for (const url of urls) {
       m++;
-      if (m > 3) continue;
+      if (m > 1) continue;
       console.log(url);
       await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 60000 });
       console.log('✅ Accessed to Page.');
@@ -113,7 +114,7 @@ const RIGHTMOVE_URL = process.env.RIGHTMOVE_URL;
       let n = 0;
       for (const item of data) {
         n++;
-        if (n > 5) continue;
+        if (n > 3) continue;
 
         console.log("link: " + item.link);
         console.log("name: " + item.name);
@@ -124,10 +125,79 @@ const RIGHTMOVE_URL = process.env.RIGHTMOVE_URL;
           const property_list = await page.$eval('div[data-test="propertyList"] div', el => el.innerText.trim());
           console.log("property_list: " + property_list);
 
+          // Click "See all properties" link to access inside
+          const see_all_propreties_link = await page.$eval('a[data-testid="propertyResultsLink"]', el => el.href);
+          await page.goto(see_all_propreties_link, { waitUntil: 'domcontentloaded', timeout: 60000 });
+
+          const properties = await page.$$('a[data-testid="property-price"]');
+          console.log(`🔎 Found ${properties.length} properties`);
+
+          const prices = [];
+          for (const proprety of properties) {
+            const price = await proprety.$eval('.PropertyPrice_price__VL65t', el => el.innerText.trim());
+            console.log("price: " + price);
+            prices.push({ price: price });
+          }
+
+          // Đầu tiên kiểm tra xem pagination có tồn tại không
+          const paginationElement = await page.$(
+            'div.Pagination_pageSelectContainer__zt0rg span:nth-child(3)'
+          );
+
+          if (paginationElement) {
+            // Nếu tồn tại, lấy nội dung rồi trích số
+            const pagination = await page.$eval(
+              'div.Pagination_pageSelectContainer__zt0rg span:nth-child(3)',
+              el => el.innerText.trim()
+            );
+            const onlyNumberMatch = pagination.match(/\d+/);
+            const totalPages = onlyNumberMatch ? onlyNumberMatch[0] : null;
+
+            console.log("pages: " + totalPages);
+            
+            for (let p = 1; p <= totalPages; p++) {
+              if (p != 1) {
+                const indexParam = `index=${(p - 1) * per_page}`;
+                const urlWithIndex = see_all_propreties_link.includes('?')
+                  ? `${see_all_propreties_link}&${indexParam}`
+                  : `${see_all_propreties_link}?${indexParam}`;
+                
+                console.log("urlWithIndex: " + urlWithIndex);
+                await page.goto(urlWithIndex, { waitUntil: 'domcontentloaded', timeout: 60000 });
+
+                const properties = await page.$$('a[data-testid="property-price"]');
+                for (const proprety of properties) {
+                  const price = await proprety.$eval('.PropertyPrice_price__VL65t', el => el.innerText.trim());
+                  // console.log("price: " + price);
+                  prices.push({ price: price });
+                }
+              }
+            }
+          }
+
+          console.log("prices: :", prices);
+
+          const priceNumbers = prices.map(p => {
+            const cleaned = p.price.replace(/[^\d.]/g, ''); // loại bỏ mọi thứ không phải số
+            return Number(cleaned);
+          });
+
+          // Tính max, min, avg
+          const max = `£${Math.max(...priceNumbers)} pcm`;
+          const min = `£${Math.min(...priceNumbers)} pcm`;
+          const avg = `£${Math.round(priceNumbers.reduce((sum, val) => sum + val, 0) / priceNumbers.length)} pcm`;
+
+          console.log(`Max: ${max.toLocaleString('en-US')}`);
+          console.log(`Min: ${min.toLocaleString('en-US')}`);
+          console.log(`Avg: ${avg.toLocaleString('en-US')}`);
+
           results.push({
             name: item.name,
             link: item.link,
             property_list,
+            max: max,
+            min: min,
+            avg: avg
           });
         } catch (err) {
           console.log(`❌ Lỗi khi xử lý link: ${item.link}`, err.message);
