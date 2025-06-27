fe66c6fc3878999df5b8d15087b6b87f51145122
Thomas Nguyen <thomas.nguyen1@vmogroup.com>
2025-06-27 17:44:45 +0700

    feat: update

--- rightmove-crawler.js
+++ rightmove-crawler.js
@@ -80,140 +80,147 @@ const RIGHTMOVE_URL = process.env.RIGHTMOVE_URL;
     let m = 0;
     for (const url of urls) {
       m++;
-      if (m > 3) continue;
+      // if (m > 3) continue;
       console.log(url);
       await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 60000 });
       console.log('✅ Accessed to Page.');
 
-      await page.waitForSelector('article section div[data-test="agent-card"] h2 a');
-      await page.waitForSelector('article section div[data-test="microsite-card"] h2 a');
-      console.log('✅ Posts loaded.');
+      const agent_card_element = await page.$(
+        'article section div[data-test="agent-card"] h2 a'
+      );
+      const microsite_card_element = await page.$(
+        'article section div[data-test="microsite-card"] h2 a'
+      );
 
-      const posts_1 = await page.$$('article section div[data-test="agent-card"] h2');
-      const posts_2 = await page.$$('article section div[data-test="microsite-card"] h2');
-      console.log(`🔎 Found ${posts_1.length} agent-card & ${posts_2.length} microsite-card.`);
-
-      const data = [];
-      for (const post of posts_1) {
-        const name = await post.$eval('a', el => el.innerText.trim());
-        const link = await post.$eval('a', el => el.href);
-        data.push({ name, link });
-      }
-
-      // 👇 Push post_2 name only (NO crawling)
-      for (const post of posts_2) {
-        try {
-          const name = await post.$eval('a', el => el.innerText.trim());
-          results.push({ name, property_list: '', max: '', min: '', avg: ''});
-        } catch (err) {
-          console.warn('⚠️ Không lấy được name từ post_2:', err.message);
+      if (microsite_card_element) {
+        const posts_2 = await page.$$('article section div[data-test="microsite-card"] h2');
+        // 👇 Push post_2 name only (NO crawling)
+        for (const post of posts_2) {
+          try {
+            const name = await post.$eval('a', el => el.innerText.trim());
+            results.push({ name, property_list: '', max: '', min: '', avg: ''});
+          } catch (err) {
+            console.warn('⚠️ Không lấy được name từ post_2:', err.message);
+          }
         }
       }
 
-      // 👇 Crawl only posts with link (from posts_1)
-      let n = 0;
-      for (const item of data) {
-        n++;
-        if (n > 3) continue;
+      console.log('✅ Posts loaded.');
 
-        console.log("link: " + item.link);
-        console.log("name: " + item.name);
+      if (agent_card_element) {
+        const posts_1 = await page.$$('article section div[data-test="agent-card"] h2');
+        const data = [];
+        for (const post of posts_1) {
+          const name = await post.$eval('a', el => el.innerText.trim());
+          const link = await post.$eval('a', el => el.href);
+          data.push({ name, link });
+        }
 
-        try {
-          let max = '';
-          let min = '';
-          let avg = '';
-          await page.goto(item.link, { waitUntil: 'domcontentloaded', timeout: 60000 });
+        // 👇 Crawl only posts with link (from posts_1)
+        let n = 0;
+        for (const item of data) {
+          n++;
+          if (n > 3) continue;
 
-          const property_list = await page.$eval('div[data-test="propertyList"] div', el => el.innerText.trim());
-          console.log("property_list: " + property_list);
+          console.log("link: " + item.link);
+          console.log("name: " + item.name);
 
-          // Click "See all properties" link to access inside
-          const see_all_propreties_link_element = await page.$(
-            'a[data-testid="propertyResultsLink"]'
-          );
-          if (see_all_propreties_link_element) {
-            const see_all_propreties_link = await page.$eval('a[data-testid="propertyResultsLink"]', el => el.href);
-            await page.goto(see_all_propreties_link, { waitUntil: 'domcontentloaded', timeout: 60000 });
+          try {
+            let max = '';
+            let min = '';
+            let avg = '';
+            await page.goto(item.link, { waitUntil: 'domcontentloaded', timeout: 60000 });
 
-            const properties = await page.$$('a[data-testid="property-price"]');
-            console.log(`🔎 Found ${properties.length} properties`);
+            const property_list = await page.$eval('div[data-test="propertyList"] div', el => el.innerText.trim());
+            console.log("property_list: " + property_list);
 
-            const prices = [];
-            for (const proprety of properties) {
-              const price = await proprety.$eval('.PropertyPrice_price__VL65t', el => el.innerText.trim());
-              console.log("price: " + price);
-              prices.push({ price: price });
-            }
-
-            // Đầu tiên kiểm tra xem pagination có tồn tại không
-            const paginationElement = await page.$(
-              'div.Pagination_pageSelectContainer__zt0rg span:nth-child(3)'
+            // Click "See all properties" link to access inside
+            const see_all_propreties_link_element = await page.$(
+              'a[data-testid="propertyResultsLink"]'
             );
+            if (see_all_propreties_link_element) {
+              const see_all_propreties_link = await page.$eval('a[data-testid="propertyResultsLink"]', el => el.href);
+              await page.goto(see_all_propreties_link, { waitUntil: 'domcontentloaded', timeout: 60000 });
 
-            if (paginationElement) {
-              // Nếu tồn tại, lấy nội dung rồi trích số
-              const pagination = await page.$eval(
-                'div.Pagination_pageSelectContainer__zt0rg span:nth-child(3)',
-                el => el.innerText.trim()
+              const properties = await page.$$('a[data-testid="property-price"]');
+              console.log(`🔎 Found ${properties.length} properties`);
+
+              const prices = [];
+              for (const proprety of properties) {
+                const price = await proprety.$eval('.PropertyPrice_price__VL65t', el => el.innerText.trim());
+                console.log("price: " + price);
+                prices.push({ price: price });
+              }
+
+              // Đầu tiên kiểm tra xem pagination có tồn tại không
+              const paginationElement = await page.$(
+                'div.Pagination_pageSelectContainer__zt0rg span:nth-child(3)'
               );
-              const onlyNumberMatch = pagination.match(/\d+/);
-              const totalPages = onlyNumberMatch ? onlyNumberMatch[0] : null;
 
-              console.log("pages: " + totalPages);
-              
-              for (let p = 1; p <= totalPages; p++) {
-                if (p != 1) {
-                  const indexParam = `index=${(p - 1) * per_page}`;
-                  const urlWithIndex = see_all_propreties_link.includes('?')
-                    ? `${see_all_propreties_link}&${indexParam}`
-                    : `${see_all_propreties_link}?${indexParam}`;
-                  
-                  console.log("urlWithIndex: " + urlWithIndex);
-                  await page.goto(urlWithIndex, { waitUntil: 'domcontentloaded', timeout: 60000 });
+              if (paginationElement) {
+                // Nếu tồn tại, lấy nội dung rồi trích số
+                const pagination = await page.$eval(
+                  'div.Pagination_pageSelectContainer__zt0rg span:nth-child(3)',
+                  el => el.innerText.trim()
+                );
+                const onlyNumberMatch = pagination.match(/\d+/);
+                const totalPages = onlyNumberMatch ? onlyNumberMatch[0] : null;
 
-                  const properties = await page.$$('a[data-testid="property-price"]');
-                  for (const proprety of properties) {
-                    const price = await proprety.$eval('.PropertyPrice_price__VL65t', el => el.innerText.trim());
-                    // console.log("price: " + price);
-                    prices.push({ price: price });
+                console.log("pages: " + totalPages);
+                
+                for (let p = 1; p <= totalPages; p++) {
+                  if (p != 1) {
+                    const indexParam = `index=${(p - 1) * per_page}`;
+                    const urlWithIndex = see_all_propreties_link.includes('?')
+                      ? `${see_all_propreties_link}&${indexParam}`
+                      : `${see_all_propreties_link}?${indexParam}`;
+                    
+                    console.log("urlWithIndex: " + urlWithIndex);
+                    await page.goto(urlWithIndex, { waitUntil: 'domcontentloaded', timeout: 60000 });
+
+                    const properties = await page.$$('a[data-testid="property-price"]');
+                    for (const proprety of properties) {
+                      const price = await proprety.$eval('.PropertyPrice_price__VL65t', el => el.innerText.trim());
+                      // console.log("price: " + price);
+                      prices.push({ price: price });
+                    }
                   }
                 }
               }
+
+              console.log("prices: :", prices);
+
+              const priceNumbers = prices.map(p => {
+                const cleaned = p.price.replace(/[^\d.]/g, ''); // loại bỏ mọi thứ không phải số
+                return Number(cleaned);
+              });
+
+              // Tính max, min, avg
+              max = `£${Math.max(...priceNumbers)} pcm`;
+              min = `£${Math.min(...priceNumbers)} pcm`;
+              avg = `£${Math.round(priceNumbers.reduce((sum, val) => sum + val, 0) / priceNumbers.length)} pcm`;
+
+              console.log(`Max: ${max.toLocaleString('en-US')}`);
+              console.log(`Min: ${min.toLocaleString('en-US')}`);
+              console.log(`Avg: ${avg.toLocaleString('en-US')}`);
             }
 
-            console.log("prices: :", prices);
-
-            const priceNumbers = prices.map(p => {
-              const cleaned = p.price.replace(/[^\d.]/g, ''); // loại bỏ mọi thứ không phải số
-              return Number(cleaned);
+            results.push({
+              name: item.name,
+              link: item.link,
+              property_list,
+              max: max,
+              min: min,
+              avg: avg
             });
-
-            // Tính max, min, avg
-            max = `£${Math.max(...priceNumbers)} pcm`;
-            min = `£${Math.min(...priceNumbers)} pcm`;
-            avg = `£${Math.round(priceNumbers.reduce((sum, val) => sum + val, 0) / priceNumbers.length)} pcm`;
-
-            console.log(`Max: ${max.toLocaleString('en-US')}`);
-            console.log(`Min: ${min.toLocaleString('en-US')}`);
-            console.log(`Avg: ${avg.toLocaleString('en-US')}`);
+          } catch (err) {
+            console.log(`❌ Lỗi khi xử lý link: ${item.link}`, err.message);
+            results.push({
+              name: item.name,
+              link: item.link,
+              error: err.message,
+            });
           }
-
-          results.push({
-            name: item.name,
-            link: item.link,
-            property_list,
-            max: max,
-            min: min,
-            avg: avg
-          });
-        } catch (err) {
-          console.log(`❌ Lỗi khi xử lý link: ${item.link}`, err.message);
-          results.push({
-            name: item.name,
-            link: item.link,
-            error: err.message,
-          });
         }
       }
     }
